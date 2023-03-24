fbd4583d8205bf8f9c3e68204dea64c2cfacf05f
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-24 15:34:52 +0100

    Bonus: Aggiunta di un secondo componente che rappresenti il jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+    <img src="../../public/vite.svg" alt="Logo of Vite JS">
+</template>
+
+<style scoped>
+    img{
+        height: 200px;
+        margin-bottom: 100px;
+    }
+</style>
